3800e022dedd3fc606f521b75e8d50ad264b82b7
jeppuka <jesper.k.kahari@gmail.com>
2023-04-04 07:50:41 +0800

    will this work

--- src/App.js
+++ src/App.js
@@ -1,42 +1,74 @@
 import React, { useState, useEffect } from 'react';
-import { VictoryChart, VictoryLine, VictoryTheme } from 'victory';
+import {VictoryChart,VictoryLine,VictoryTheme,VictoryAxis} from 'victory';
+import './App.css';
 
-const BitcoinPriceGraph = () => {
+const BitcoinPriceGraph = ({ ticker }) => {
   const [bitcoinPrices, setBitcoinPrices] = useState(null);
 
   useEffect(() => {
     const fetchBitcoinPrices = async () => {
       const response = await fetch(
-        'https://api.coingecko.com/api/v3/coins/ethereum/market_chart?vs_currency=gbp&days=14'
+        `https://api.coingecko.com/api/v3/coins/${ticker}/market_chart?vs_currency=gbp&days=30`
       );
       const data = await response.json();
       setBitcoinPrices(data.prices);
     };
 
     fetchBitcoinPrices();
-  }, []);
+  }, [ticker]);
 
   const transformData = (data) => {
     return data.map((price) => ({
-      y: price[1],
+      y: price[1]
     }));
   };
 
   return (
+    <div >
+      <h1>{`${ticker.toUpperCase()} Price Graph (GBP)`}</h1>
+      {bitcoinPrices && (
+        <VictoryChart
+          theme={VictoryTheme.material}
+          style={{
+            axisLabel: { fontSize: 5 },
+          }}
+        >
 
-      <div className="App">
-        <header className="App-header">
-          <div>
-            <h1>Price Graph (GBP)</h1>
-            {bitcoinPrices && (
-              <VictoryChart theme={VictoryTheme.material} style = {{fontSize: 2}}width={400} height={400}>
-                <VictoryLine data={transformData(bitcoinPrices)}style={{ data: {strokeWidth: 0.5} }} />
-              </VictoryChart>
-            )}
-          </div>
-        </header>
+          <VictoryAxis
+            dependentAxis
+            label="Price (GBP)"
+            style={{
+              axisLabel: { padding: 40 },
+            }}
+          />
+          <VictoryLine data={transformData(bitcoinPrices) } style={{ data: { strokeWidth: 0.5} }} />
+        </VictoryChart>
+      )}
     </div>
   );
 };
 
-export default BitcoinPriceGraph;
+const TickerSearch = () => {
+  const [ticker, setTicker] = useState('bitcoin');
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = () => {
+    setTicker(searchTerm.toLowerCase());
+  };
+
+  return (
+    <div className='App'>
+      <h1>Crypto Price Graph</h1>
+      <input
+        type="text"
+        placeholder="Enter a ticker symbol (e.g. bitcoin)"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+      <BitcoinPriceGraph ticker={ticker} />
+    </div>
+  );
+};
+
+export default TickerSearch;
